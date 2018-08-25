29cae4477703c28e489d874b9e81a457385c4e91
haerfest <wouter.hobers+github@gmail.com>
2018-08-25 19:06:49 +0200

    Rewrite, incomplete.

--- uef2wave.py
+++ uef2wave.py
@@ -1,352 +1,330 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
-"""
-	Name		: uef2wave
-	Author		: Wouter Hobers
-	Created		: May 21 2001
-	Last edited	: May 23 2001
-	Purpose		: Convert tape blocks of UEF files to WAVE files so they can be
-			  recorded on tape and used with a real Electron.
-	WWW		: http://www.fmf.nl/~xaviar/acorn/
-	Email		: xaviar@fmf.nl
-"""
+from __future__ import print_function
 
+from argparse import ArgumentParser
+from struct import unpack
 
-import sys, math, gzip
 
-if sys.platform == "mac":
-	import macfs
+class Chunk(object):
+    def __init__(self, identifier, data):
+        self.identifier = identifier
+        self.data = data
 
 
-global TRUE
-global FALSE
-global BAUD
+def chunks(ueffile):
+    with open(ueffile, 'rb') as uef:
+        # Check the magic value, indicating it's a UEF file.
+        magic = uef.read(10)
+        if magic != b'UEF File!\x00':
+            raise Exception('{}: not a UEF file'.format(ueffile))
 
-TRUE  = 1 == 1
-FALSE = not TRUE
+        # Skip over the UEF version.
+        uef.read(2)
 
-BAUD = 1000000.0/(16.0*52.0)
+        while True:
+            # Read the chunk identifier and test for EOF.
+            identifier = uef.read(2)
+            if len(identifier) == 0:
+                break
+            identifier = unpack('<H', identifier)[0]
 
+            # Read the length and data bytes.
+            length = unpack('<I', uef.read(4))[0]
+            data = uef.read(length)
 
-class UEFParser:
+            yield Chunk(identifier, data)
 
-	
-	tapeID     = 0x0100
-	highToneID = 0x0110
-	gapID      = 0x0112
-	
-	tapeIDList = [highToneID, gapID, tapeID]
 
-	startBit = "0"
-	stopBit  = "1"
-
-	buffer = []
-	pos    = 0
-
-
-	def __init__(self,buffer):
+class Recordable(object):
+    def record(self, recorder):
+        raise Exception('{}; record() not implemented'.format(type(self)))
 
-		self.buffer = buffer
 
+class LowPulse(Recordable):
+    def __init__(self, bit):
+        self.bit = bit
 
-	def WordAt(self,buffer,pos):
-		
-		if pos + 1 >= len(buffer):
-			return 0
+    def __repr__(self):
+        return '<LowPulse {}>'.format(self.bit)
 
-		return ord(buffer[pos]) + ord(buffer[pos+1]) * 256
-	
+    def record(self, recorder):
+        recorder.low_pulse(n=self.bit + 1)
 
-	def LongAt(self,buffer,pos):
 
-		if pos + 3 >= len(buffer):
-			return 0
-				
-		return self.WordAt(buffer,pos) + self.WordAt(buffer,pos+2) * 256 * 256
-			
+class HighPulse(Recordable):
+    def __init__(self, bit):
+        self.bit = bit
 
-	def ReadBlockID(self):
-		
-		id = self.WordAt(self.buffer, self.pos)
-		self.pos = self.pos + 2
-		return id
-		
+    def __repr__(self):
+        return '<HighPulse {}>'.format(self.bit)
 
-	def FindTapeBlock(self):
-		
-		id = -1
+    def record(self, recorder):
+        recorder.high_pulse(n=self.bit + 1)
 
-		while self.pos < len(self.buffer):
-			
-			id = self.ReadBlockID()
-			length = self.LongAt(self.buffer, self.pos)
-			
-			if id in self.tapeIDList:
-				break
-				
-			self.pos = self.pos + 4 + length
 
-		return id in self.tapeIDList, id
-	
+class SlowCycle(Recordable):
+    '''
+    Represents a single slow cycle.
+    '''
+    def __repr__(self):
+        return '<SlowCycle>'
 
-	def ToBitString(self,buffer):
-		
-		power2 = [1,2,4,8,16,32,64,128]
+    def record(self, recorder):
+        recorder.low_pulse()
+        recorder.high_pulse()
 
-		s = ""
 
-		pos = 0
-		while pos < len(buffer):
+class FastCycle(Recordable):
+    '''
+    Represents a single fast cycle.
+    '''
+    def __repr__(self):
+        return '<FastCycle>'
 
-			s = s + self.startBit
-
-			for i in range(0,8):
-				if (ord(buffer[pos]) & power2[i]):
-					s = s + "1"
-				else:
-					s = s + "0"
-
-			s = s + self.stopBit
-			
-			pos = pos + 1
-
-		return s
-
-
-	def ReadTapeBlock(self,id):
-		
-		length   = self.LongAt(self.buffer,self.pos)
-		self.pos = self.pos + 4
-	
-		if id == self.tapeID:
-			ret = self.ToBitString(self.buffer[self.pos:self.pos + length])
-		elif id == self.gapID:
-			ms  = self.WordAt(self.buffer,self.pos)
-			ret = int(ms * (BAUD/1000.0)) * " "
-		elif id == self.highToneID:
-			ms  = self.WordAt(self.buffer,self.pos)
-			ret = int(ms * (BAUD/1000.0)) * "1"			
-		
-		self.pos = self.pos + length
-		
-		return ret
-
-	
-	def ReadAllTapeBlocks(self):
-		
-		ret = ""
-		
-		found, id = self.FindTapeBlock()
-		while found:
-			ret       = ret + self.ReadTapeBlock(id)
-			found, id = self.FindTapeBlock()
-		
-		return ret
-
-
-class WaveWriter:
-	
-
-	maxVolumeLevel = 127
-	minVolumeLevel = 0
-
-	fp     = None
-	buffer = ""	
-
-	sine1200Hz = ""
-	sine2400Hz = ""
-
-
-	def __init__(self,fp,buffer):
-
-		self.fp     = fp
-		self.buffer = buffer
-		
-		self.CalcSine()
-	
-
-	def Str2(self,i):
-		
-		return chr(i & 0x00FF) + chr((i & 0xFF00) >> 8)
-		
-	
-	def Str4(self,i):
-		
-		return self.Str2(i) + self.Str2((i & 0xFFFF0000) >> 16)
-		
-
-	def SetVolumeMin(self,volume):
-		
-		if volume > 0:
-			self.minVolumeLevel = volume & 0xFF
-
-
-	def SetVolumeMax(self,volume):
-
-		if volume > 0:	
-			self.maxVolumeLevel = volume & 0xFF
-
-
-	def CalcSine(self):
-
-		self.sine1200Hz = self.sine2400Hz = ""
-		
-		amp = (self.maxVolumeLevel - self.minVolumeLevel) / 2.0
-		cy  = self.minVolumeLevel + amp
-
-		dx = 2.0 * math.pi / 18.0		
-		for i in range(0,18):
-			self.sine1200Hz = self.sine1200Hz + chr(int(cy + amp * math.sin(i * dx)))
-		
-		dx = 4.0 * math.pi / 18.0
-		for i in range(0,18):
-			self.sine2400Hz = self.sine2400Hz + chr(int(cy + amp * math.sin(i * dx)))
-
-
-	def WriteWaveHeader(self):
-	
-		bufferLen = len(self.buffer) * 18
-		
-		self.fp.write("RIFF")
-		self.fp.write(self.Str4(bufferLen + 4 + 4 + 4 + 16 + 4 + 4))
-		self.fp.write("WAVE")
-		
-		self.fp.write("fmt ")
-		self.fp.write(self.Str4(16))
-		self.fp.write(self.Str2(1))
-		self.fp.write(self.Str2(1))
-		self.fp.write(self.Str4(22050))
-		self.fp.write(self.Str4(22050))
-		self.fp.write(self.Str2(0))
-		self.fp.write(self.Str2(8))
-
-		self.fp.write("data")
-		self.fp.write(self.Str4(bufferLen))
-		
-
-	def WriteBit(self,ch):
-
-		if ch == "0":
-			
-			self.fp.write(self.sine1200Hz)
-			
-		elif ch == "1":
-			
-			self.fp.write(self.sine2400Hz)
-					
-		elif ch == " ":
-			
-			self.fp.write(18 * chr(self.minVolumeLevel))
-
-
-	def WriteWave(self):
-		
-		self.WriteWaveHeader()
-		
-		pos = 0
-		
-		while pos < len(self.buffer):
-			self.WriteBit(self.buffer[pos])
-			pos = pos + 1
-
-
-class Convertor:
-
-
-	inFile  = ""
-	outFile = ""
-
-	volumeMin = 0
-	volumeMax = 0
-
-
-	def __init__(self,inFile,outFile):
-
-		self.inFile  = inFile
-		self.outFile = outFile
-		
-	
-	def SetVolume(self,volume):
-	
-		self.volumeMax = volume
-
-
-	def SetVolumeRange(self,min,max):
-		
-		self.volumeMin = min
-		self.volumeMax = max
-
-
-	def Convert(self):
-
-		try:
-			fpIn = open(self.inFile, "rb")
-		except IOError:
-			raise IOError, "Couldn't open file: " + self.inFile
-		
-		if fpIn.read(10) != "UEF File!\000":
-			fpIn.close()
-
-			try:
-				fpIn = gzip.open(self.inFile, "rb")
-				if fpIn.read(10) != "UEF File!\000":
-					fpIn.close()
-					raise IOError, "Not a UEF file: " + self.inFile
-
-			except IOError:
-				raise IOError, "Not a UEF file: " + self.inFile
-
-		fpIn.read(2)
-		buffer = fpIn.read()
-		fpIn.close()
-		
-		try:
-			fpOut = open(self.outFile, "wb")
-		except IOError:
-			raise IOError, "Couldn't open file: " + self.outFile
-		
-		parser    = UEFParser(buffer)
-		bitStream = parser.ReadAllTapeBlocks()
-				
-		waver = WaveWriter(fpOut,bitStream)
-		if self.volumeMin > 0: waver.SetVolumeMin(self.volumeMin)
-		if self.volumeMax > 0: waver.SetVolumeMax(self.volumeMax)
-		waver.WriteWave()
-		
-		fpOut.close()
-
-
-# main
-
-if sys.platform == "mac":
-
-	# Mac has no command-line (pre OS X)
-	
-	fsSpecIn, ok = macfs.StandardGetFile()
-	if not ok: sys.exit(1)
-	
-	fsSpecOut, ok = macfs.StandardPutFile("Save wave file as:")
-	if not ok: sys.exit(1)
-	
-	convertor = Convertor(fsSpecIn.as_pathname(), fsSpecOut.as_pathname())
-	
-else:
-
-	# a command-line is assumed here
-	
-	if len(sys.argv) != 3:
-		print "Syntax:", sys.argv[0], "<inputfile.uef> <outputfile.wav>"
-		sys.exit(1)
-	
-	convertor = Convertor(sys.argv[1], sys.argv[2])
-	
-convertor.SetVolumeRange(0x30, 0xC0)
-
-try:
-	convertor.Convert()
-	print "All done."
-except IOError, msg:
-	print "An error occured:", msg
-	sys.exit(1)
-
-sys.exit()
+    def record(self, recorder):
+        recorder.low_pulse(n=2)
+        recorder.high_pulse(n=2)
+
+
+class ZeroBit(SlowCycle):
+    '''
+    Represents a zero-bit.
+    '''
+    def __repr__(self):
+        return '<Bit 0>'
+
+
+class OneBit(Recordable):
+    '''
+    Represents a one-bit.
+    '''
+    def __repr__(self):
+        return '<Bit 1>'
+
+    def record(self, recorder):
+        recorder.low_pulse(n=2)
+        recorder.high_pulse(n=2)
+        recorder.low_pulse(n=2)
+        recorder.high_pulse(n=2)
+
+
+class StartBit(ZeroBit):
+    '''
+    Represents a start bit.
+    '''
+    def __repr__(self):
+        return '<StartBit>'
+
+
+class StopBit(OneBit):
+    '''
+    Represents a stop bit.
+    '''
+    def __repr__(self):
+        return '<StopBit>'
+
+
+class Carrier(Recordable):
+    '''
+    Represents a carrier tone of a certain cycle count.
+    '''
+    def __init__(self, cycle_count):
+        self.cycle_count = cycle_count
+
+    def __repr__(self):
+        return '<Carrier {} cycles>'.format(self.cycle_count)
+
+    def record(self, recorder):
+        for _ in range(self.cycle_count):
+            recorder.low_pulse(n=2)
+            recorder.high_pulse(n=2)
+
+
+class IntegerGap(Recordable):
+    def __init__(self, n):
+        self.n = n
+
+    def __repr__(self):
+        return '<IntegerGap {}>'.format(self.n)
+
+    def record(self, recorder):
+        for _ in range(self.n):
+            recorder.low_pulse(n=2, amplitude=0)
+            recorder.high_pulse(n=2, amplitude=0)
+
+
+class FloatGap(Recordable):
+    def __init__(self, seconds):
+        self.seconds = seconds
+
+    def __repr__(self):
+        return '<FloatGap {:.1f} sec>'.format(self.seconds)
+
+    def record(self, recorder):
+        cycle_count = int(round(self.seconds / (2 * recorder.frequency)))
+        for _ in range(cycle_count):
+            recorder.low_pulse(n=2, amplitude=0)
+            recorder.high_pulse(n=2, amplitude=0)
+
+
+class BaseFrequencyChange(Recordable):
+    def __init__(self, frequency):
+        self.frequency = frequency
+
+    def __repr__(self):
+        return '<BaseFrequencyChange {:.1f} Hz>'.format(self.frequency)
+
+    def record(self, recorder):
+        recorder.frequency = self.frequency
+
+
+class PhaseChange(Recordable):
+    def __init__(self, phase):
+        self.phase = phase
+
+    def __repr__(self):
+        return '<PhaseChange {} deg>'.format(self.phase)
+
+    def record(self, recorder):
+        recorder.phase = self.phase
+
+
+class Transformer(object):
+    def __init__(self):
+        self.ignored = set()
+
+    def transform(self, chunk):
+        method = 'transform_{:04x}'.format(chunk.identifier)
+        transformer = getattr(self, method, None)
+        if transformer and callable(transformer):
+            recordables = transformer(chunk.data)
+            if recordables:
+                return recordables
+
+        self.ignored.add(chunk.identifier)
+        return []
+
+    def transform_0100(self, data):
+        '''
+        Implicit start/stop bit tape data block.
+        '''
+        return [StartBit()] + self.bits(data) + [StopBit()]
+
+    def transform_0110(self, data):
+        '''
+        Carrier tone.
+        '''
+        cycle_count = unpack('<H', data)[0]
+        return [Carrier(cycle_count)]
+
+    def transform_0111(self, data):
+        '''
+        Carrier tone with dummy byte.
+        '''
+        length1, length2 = unpack('<HH', data)
+        return [Carrier(length1), StartBit()] + self.bits(b'\xAA') + [StopBit(), Carrier(length2)]
+
+    def transform_0112(self, data):
+        '''
+        Integer gap.
+        '''
+        n = unpack('<H', data)[0]
+        return [IntegerGap(n)]
+
+    def transform_0113(self, data):
+        '''
+        Change of base frequency.
+        '''
+        frequency = unpack('<f', data)[0]
+        return [BaseFrequencyChange(frequency)]
+
+    def transform_0114(self, data):
+        '''
+        Security cycles.
+        '''
+        upper, lower = unpack('<HB', data[:3])
+        cycle_count = upper << 8 + lower
+
+        recordables = [FastCycle() if bit.value else SlowCycle() for _, bit in zip(range(cycle_count), self.bits(data[5:]))]
+
+        if recordables and data[3] == b'P':
+            recordables[0] = HighPulse(recordables[0].value)
+        if recordables and data[4] == b'P':
+            recordables[-1] = LowPulse(recordables[-1].value)
+
+        return recordables
+
+    def transform_0115(self, data):
+        '''
+        Phase change.
+        '''
+        phase = unpack('<H', data)[0]
+        return [PhaseChange(phase)]
+
+    def transform_0116(self, data):
+        '''
+        Floating point gap.
+        '''
+        seconds = unpack('<f', data)[0]
+        return [FloatGap(seconds)]
+
+    def bits(self, bytes):
+        return [OneBit() if byte & (1 << i) else ZeroBit() for byte in bytes for i in range(8)]
+
+
+class Recorder(object):
+    def __init__(self):
+        self._frequency = 1200.0
+        self._baud = 1200
+        self._phase = 180
+        self._update = True
+
+    def set_frequency(self, frequency):
+        self._frequency = frequency
+        self._update = True
+
+    def get_frequency(self):
+        return self._frequency
+
+    frequency = property(get_frequency, set_frequency)
+
+    def set_phase(self, phase):
+        self._phase = phase
+        self._update = True
+
+    def get_phase(self):
+        return self._phase
+
+    phase = property(get_phase, set_phase)
+
+    def low_pulse(self, n=1, amplitude=1.0):
+        pass
+
+    def high_pulse(self, n=1, amplitude=1.0):
+        pass
+
+
+def parse_arguments():
+    parser = ArgumentParser()
+    parser.add_argument('ueffile', help='the UEF file to convert')
+    return parser.parse_args()
+
+
+def main():
+    args = parse_arguments()
+
+    recordables = []
+    transformer = Transformer()
+    for chunk in chunks(args.ueffile):
+        recordables.extend(transformer.transform(chunk))
+
+    print('ignored: ' + ', '.join(['&{:04x}'.format(i) for i in transformer.ignored]))
+
+    recorder = Recorder()
+    for r in recordables:
+        r.record(recorder)
+
+
+if __name__ == '__main__':
+    main()
