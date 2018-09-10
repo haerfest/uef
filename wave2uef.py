b7d1a5536f1fbf418fe177a23723feaa1dfd67dc
haerfest <wouter.hobers+github@gmail.com>
2018-09-11 01:03:42 +0200

    Correctly deal with cycles passing zero.

--- wave2uef.py
+++ wave2uef.py
@@ -6,9 +6,8 @@ import io
 import sys
 
 
-def get_sample(stream, threshold=0.1):
-    sample = unpack('<h', stream.read(2))[0] / 32768.0
-    return sample
+def get_sample(stream):
+    return unpack('<h', stream.read(2))[0] / 32768.0
 
 
 def sign(x):
@@ -19,24 +18,37 @@ def sign(x):
     return 0
 
 
+def unget(stream, length=1):
+    stream.seek(-length * 2, io.SEEK_CUR)
+
+
 def get_pulse(stream):
     first = get_sample(stream)
-
     length = 1
+
+    # Figure out the sign of our pulse. If the first sample is zero,
+    # we look at the next to figure out our sign, or whether we are
+    # silence.
+    sgn = sign(first)
+    if sgn == 0:
+        subsequent = get_sample(stream)
+        length += 1
+        if sign(subsequent) != 0:
+            sgn = sign(subsequent)
+
+    # Consume the pulse.
     subsequent = get_sample(stream)
-    while sign(subsequent) == sign(first):
+    while sign(subsequent) == sgn:
         length += 1
         subsequent = get_sample(stream)
 
-    # Unread the last sample, which is not part of this pulse.
-    stream.seek(-2, io.SEEK_CUR)
+    # If the final sample is not zero, then its sign has flipped
+    # and we unread it.
+    if sign(subsequent) != 0:
+        unget(stream)
 
-    # print('pulse ({},{})'.format(sign(first), length))
+    # print('{},{}'.format(sign(first), length), flush=True)
     return sign(first), length
-
-
-def unget_pulse(stream, length):
-    stream.seek(-length * 2, io.SEEK_CUR)
 
 
 def skip_header(stream):
@@ -61,54 +73,29 @@ def wait_first_pulse(stream):
     while length not in [8, 9, 10, 16, 17, 18, 19] or sgn >= 0:
         sgn, length = get_pulse(stream)
 
-    unget_pulse(stream, length)
+    unget(stream, length)
     return 'ok'
 
 
-def read_cycle(stream):
+def read_cycle(stream, expected_freq):
     sgn1, length1 = get_pulse(stream)
     sgn2, length2 = get_pulse(stream)
 
     if sgn2 != -sgn1:
         # Phase is wrong.
-        unget_pulse(stream, length2)
+        unget(stream, length1 + length2)
         return 'phase'
 
-    if abs(length1 - length2) > 2:
-        print('pulses {} and {}'.format(length1, length2))
+    ratio = max(length1, length2) / min(length1, length2)
+    if ratio > 1.6:
         # Pulse lengths don't match.
-        unget_pulse(stream, length2)
+        unget(stream, length1 + length2)
         return 'pulse lengths'
 
-    return ('ok', length1, length2)
-
-
-def read_slow_cycle(stream):
-    result = read_cycle(stream)
-    if isinstance(result, str):
-        return result
-
-    lengths = result[1:]
-    frequency = 44100 / sum(lengths)
-    if abs(frequency - 1200) > 500:
-        # Not a slow cycle.
-        unget_pulse(stream, sum(lengths))
-        return 'not slow'
-
-    return 'ok'
-
-
-def read_fast_cycle(stream):
-    result = read_cycle(stream)
-    if isinstance(result, str):
-        return result
-
-    lengths = result[1:]
-    frequency = 44100 / sum(lengths)
-    if abs(frequency - 2400) > 500:
-        # Not a fast cycle.
-        unget_pulse(stream, sum(lengths))
-        return 'not fast'
+    freq = 44100 / (length1 + length2)
+    if abs(freq - expected_freq) > 500:
+        unget(stream, length1 + length2)
+        return 'not {}'.format(expected_freq)
 
     return 'ok'
 
@@ -118,44 +105,43 @@ def read_carrier(stream):
     # That's at most nine one-bits, or eighteen fast cycles, in a row. If we
     # read nineteen or more fast cycles, we are in the carrier signal.
     for _ in range(19):
-        result = read_fast_cycle(stream)
+        result = read_cycle(stream, 2400)
         if result != 'ok':
             return result
 
     # Consume the remainder of the carrier.
-    while read_fast_cycle(stream) == 'ok':
+    while read_cycle(stream, 2400) == 'ok':
         pass
 
     return 'ok'
 
 
+def read_fast_cycle(stream):
+    return read_cycle(stream, 2400)
+
+
 def read_start_bit(stream):
-    return read_slow_cycle(stream)
+    return read_cycle(stream, 1200)
 
 
 def read_byte(stream):
     bits = []
 
     for _ in range(8):
-        result = read_cycle(stream)
-        if isinstance(result, str):
-            return result
-
-        length1, length2 = result[1:]
-        frequency = 44100 / (length1 + length2)
-        if abs(frequency - 1200) <= 500:
+        result = read_cycle(stream, 1200)
+        if result == 'ok':
             # Slow cycle, a zero-bit.
             bits.append(0)
             continue
 
-        if abs(frequency - 2400) > 500:
-            # Not a fast cycle.
-            return 'not fast'
-
-        result = read_fast_cycle(stream)
+        # Need two fast cycles for a one-bit.
+        result = read_cycle(stream, 2400)
         if result != 'ok':
-            # Need another fast cycle for a one-bit.
-            return 'not fast'
+            return result
+
+        result = read_cycle(stream, 2400)
+        if result != 'ok':
+            return result
 
         bits.append(1)
 
@@ -164,70 +150,87 @@ def read_byte(stream):
         asc += bit * 2**index
 
     if 32 <= asc <= 127:
-        print(chr(asc), end='')
+        print(chr(asc), end='', flush=True)
     else:
-        print('<{:02x}>'.format(asc), end='')
+        print('.', end='', flush=True)
 
     return 'ok'
 
 
 def read_stop_bit(stream):
-    result = read_fast_cycle(stream)
+    result = read_cycle(stream, 2400)
     if result != 'ok':
         return result
 
-    result = read_fast_cycle(stream)
+    result = read_cycle(stream, 2400)
     if result != 'ok':
         return result
 
     return 'ok'
 
 
-stream = skip_header(sys.stdin.buffer)
-stream_pos = 0
+def read_carrier_or_start_bit(stream):
+    result = read_carrier(stream)
+    if result == 'ok':
+        return 'carrier'
+
+    result = read_start_bit(stream)
+    if result == 'ok':
+        return 'start bit'
+
+    return 'no carrier or start bit'
+
+
+def start_bit_or_end(stream):
+    result = read_start_bit(stream)
+    if result == 'ok':
+        return 'start bit'
+
+    print(result)
+    print(get_pulse(stream))
+    print(get_pulse(stream))
+    return 'end'
+
 
 states = {
     'start': (wait_first_pulse, {
-        'ok': 'read_fast_cycle',
+        'ok': 'carrier',
     }),
-    'read_fast_cycle': (read_fast_cycle, {
-        'phase': 'start',
-        'pulse lengths': 'error',
-        'ok': 'read_carrier',
-        '*': 'read_fast_cycle',
+    'carrier': (read_carrier, {
+        'ok': 'start bit',
     }),
-    'read_carrier': (read_carrier, {
-        'ok': 'read_start_bit',
-        '*': 'read_fast_cycle',
+    'start bit': (read_start_bit, {
+        'ok': 'byte',
     }),
-    'read_start_bit': (read_start_bit, {
-        'ok': 'read_byte',
-        '*': 'error',
+    'byte': (read_byte, {
+        'ok': 'stop bit',
     }),
-    'read_byte': (read_byte, {
-        'ok': 'read_stop_bit',
-        '*': 'error'
+    'stop bit': (read_stop_bit, {
+        'ok': 'carrier or start bit',
     }),
-    'read_stop_bit': (read_stop_bit, {
-        'ok': 'read_start_bit',
-        '*': 'error',
+    'carrier or start bit': (read_carrier_or_start_bit, {
+        'carrier': 'start bit or end',
+        'start bit': 'byte',
+    }),
+    'start bit or end': (start_bit_or_end, {
+        'start bit': 'byte',
+        'end': 'done',
     })
 }
 
+stream = skip_header(sys.stdin.buffer)
 state = 'start'
+
 while True:
     # print('state={}'.format(state))
     fn, transitions = states[state]
     result = fn(stream)
-    if result in transitions:
-        next_state = transitions[result]
-    else:
-        next_state = transitions['*']
-
-    if next_state == 'done':
-        break
-    if next_state == 'error':
+    if result not in transitions:
         raise Exception(
             'Error: result "{}" in state "{}" at pos {}'.format(
                 result, state, stream.tell()))
+
+    next_state = transitions[result]
+    if next_state == 'done':
+        break
     state = next_state
